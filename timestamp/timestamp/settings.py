0535609abb76d933f0835b2240a81e4beae569b9
nayoung <nayoung.tech@gmail.com>
2021-06-20 23:51:55 +0900

    Change sqlite to postgreql

--- timestamp/timestamp/settings.py
+++ timestamp/timestamp/settings.py
@@ -10,11 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# dotenv
+ENV = os.environ['ENV']
+dotenv_path = os.path.join(BASE_DIR, f'{ENV}.env')
+load_dotenv(dotenv_path, verbose=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -75,8 +81,12 @@ WSGI_APPLICATION = 'timestamp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['DB_NAME'],
+            'USER': os.environ['DB_USER'],
+            'PASSWORD': os.environ['DB_PASSWORD'],
+            'HOST': os.environ['DB_HOST'],
+            'PORT': os.environ['DB_PORT'],
     }
 }
 
